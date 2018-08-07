8d5b634901da672025c54d65a90c6568598565c6
rgrb <rougerbaptistejob@gmail.com>
2018-08-07 19:03:01 +0200

    New version: with rendom chestsystem

---
+++ ClashHeroesXcode/Model/Chest/BlankChest.swift
@@ -0,0 +1,9 @@
+//
+//  BlankChest.swift
+//  ClashHeroesXcode
+//
+//  Created by macmini on 07/08/2018.
+//  Copyright © 2018 macmini. All rights reserved.
+//
+
+import Foundation
---
+++ ClashHeroesXcode/Model/Weapons/errorWep.swift
@@ -0,0 +1,9 @@
+//
+//  errorWep.swift
+//  ClashHeroesXcode
+//
+//  Created by macmini on 07/08/2018.
+//  Copyright © 2018 macmini. All rights reserved.
+//
+
+import Foundation
